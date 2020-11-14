43b4ddec538a54d4082fb4e597679ca4a42303bc
Ade Pangestu <adepanges@gmail.com>
2020-11-15 05:41:33 +0700

    copy file

--- deployment/copyStaticAssets.js
+++ deployment/copyStaticAssets.js
@@ -6,5 +6,10 @@ const shell = require("shelljs");
 shell.mkdir("-p", "dist/public");
 shell.mkdir("-p", "dist/resources/views");
 
+// copy all vue assets
 shell.cp("-R", "src/resources/frontend/dist/*", "dist/public");
+
+// copy for views render by express
+shell.cp("src/resources/views/400.njk", "dist/resources/views");
+shell.cp("src/resources/views/500.njk", "dist/resources/views");
 shell.cp("src/resources/frontend/dist/index.html", "dist/resources/views");
