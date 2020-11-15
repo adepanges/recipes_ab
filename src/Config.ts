5b920b76f6cec160058e318b1edca40838910e5c
Ade Pangestu <adepanges@gmail.com>
2020-11-16 02:25:31 +0700

    fix FirebaseAppError: Failed to parse private key: Error: Invalid PEM formatted message

--- src/Config.ts
+++ src/Config.ts
@@ -52,7 +52,7 @@ if(!firebaseConfig) {
         "type": process.env["SECRET_FIREBASE_TYPE"] || "",
         "project_id": process.env["SECRET_FIREBASE_PROJECT_ID"] || "",
         "private_key_id": process.env["SECRET_FIREBASE_PRIVATE_KEY_ID"] || "",
-        "private_key": process.env["SECRET_FIREBASE_PRIVATE_KEY"] || "",
+        "private_key": (process.env["SECRET_FIREBASE_PRIVATE_KEY"] || "").replace(/\\n/g, '\n'),
         "client_email": process.env["SECRET_FIREBASE_CLIENT_EMAIL"] || "",
         "client_id": process.env["SECRET_FIREBASE_CLIENT_ID"] || "",
         "auth_uri": process.env["SECRET_FIREBASE_AUTH_URI"] || "",
